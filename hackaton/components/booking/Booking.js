e21a3d8491c6331939490ba360ee83c7675751c6
Compl3x13 <steamds13@gmail.com>
2022-10-19 18:08:27 +0100

    Added css for booking.js

--- hackaton/components/booking/Booking.js
+++ hackaton/components/booking/Booking.js
@@ -2,51 +2,47 @@ import styles from "./Booking.module.css";
 
 export default function Booking() {
   return (
-    <div
-      className="booking"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <h1> Booking Details</h1>
-      <br />
-      <div className="bookingId">
-        <form>
-          Name:
-          <br />
-          <input id="username" name="username" placeholder="usrename" />
-          <br />
-          Email:
-          <br />
-          <input id="Email" name="email" placeholder="email" />
-          <br />
-          Exercise:
-          <br />
-          <input type="checkbox" />
-          Muscles
-          <br />
-          <input type="checkbox" />
-          Shoulders <br />
-          <input type="checkbox" />
-          Leg day
-          <br />
-          <input type="checkbox" />
-          Other
-          <br />
-          <input id="details" name="details" />
-          <br />
-          <br />
-          <button
-            type="submit"
-            onClick={() => alert("Appointment Booked ")}
-            className={styles.alert}
-          >
-            Book
-          </button>
-        </form>
+    <div className={styles.outerDiv}>
+
+      <div className={styles.innerDiv}>
+        <h1> Booking Details</h1>
+        <br />
+        <div className="bookingId">
+          <form>
+            Name:
+            <br />
+            <input id="username" name="username" placeholder="username" />
+            <br />
+            Email:
+            <br />
+            <input id="Email" name="email" placeholder="email" />
+            <br />
+            Exercise:
+            <br />
+            <input type="checkbox" />
+            Muscles
+            <br />
+            <input type="checkbox" />
+            Shoulders <br />
+            <input type="checkbox" />
+            Leg day
+            <br />
+            <input type="checkbox" />
+            Other
+            <br />
+            <input id="details" name="details" />
+            <br />
+            <br />
+            <button
+              type="submit"
+              onClick={() => alert("Appointment Booked ")}
+              className={styles.alert}
+            >
+              Book
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </div >
   );
 }
