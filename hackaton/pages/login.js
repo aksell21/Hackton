7ee3b7b9f9e4603c5eac6f7903f5d35a9e40300e
Compl3x13 <steamds13@gmail.com>
2022-10-19 17:11:45 +0100

    Added css for Registration and changed some of Login's css

--- hackaton/pages/login.js
+++ hackaton/pages/login.js
@@ -1,8 +1,5 @@
-
-import Buttons from '../components/buttons/Buttons'
 import Header from '../components/header/Header'
 import Login from '../components/login/Login'
-import Register from '../components/registration/Registration'
 import styles from '../styles/Home.module.css'
 
 export default function loginPage() {
