6fbd672117f229056d05a875ff542e6dcf29b7bc
Compl3x13 <steamds13@gmail.com>
2022-10-19 13:46:38 +0100

    First versions of Buttons and Header

--- hackaton/components/buttons/Buttons.js
+++ hackaton/components/buttons/Buttons.js
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import { useState } from "react"
+import styles from "./Buttons.module.css"
 
 // Button is missing it's styling, but it can be easily be edited as we need. Can be made primary or secondary button.
+
+//Added some basic CSS rules. - Sabino
 
 const Buttons = ({ children, ...props }) => {
   return (
     <div>
-      <button {...props} className="button">
+      <button {...props} className={styles.button}>
         {children}
       </button>
     </div>
