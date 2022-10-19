b062959e0a08484b1c2a5114276f8e5ac6c44f2d
Quicco <quicco.sousa@gmail.com>
2022-10-19 15:52:27 +0100

    Added table

--- hackaton/components/calendar/WeeklyCalendar.js
+++ hackaton/components/calendar/WeeklyCalendar.js
@@ -1,4 +1,70 @@
+import styles from "./WeeklyCalendar.module.css"
+import React, { useState} from "react"
+
 const WeeklyCalendar = () => {
-  return <div></div>;
+
+    const [date, setDate] = useState(new Date());
+    //Change view to weekly
+    
+    
+    return (
+        <div><form>
+            {/*  <div><p>20/10/2022</p></div> */}
+            <table className={styles.table}>
+                <tr id="tr" > <th rowSpan="2">Timings</th><th colspan="5">WeekDays</th></tr>
+                <tr>
+                    <td id="td"><p>Monday</p></td>
+                    <td id="td"><p>Tuesday</p></td>
+                    <td id="td"><p>Wednesday</p></td>
+                    <td id="td"><p>Thursday</p></td>
+                    <td id="td"><p>Friday</p></td>
+                    </tr>
+                <tr>
+                    <td id="td"><p>9 to 10</p></td>
+                    <td id="td"></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    </tr>
+                <tr>
+                    <td id="td"><p>10 to 11</p></td>
+                    <td id="7" onClick={()=> console.log("hey key 7")}></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    </tr>
+                <tr>
+                    <td id="td"><p>Segunda</p></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    </tr>
+                <tr>
+                    <td id="td"><p>Segunda</p></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    </tr>
+                <tr>
+                    <td id="td"><p>Segunda</p></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    </tr>
+                     
+               
+            </table>
+            </form>
+        </div>
+    )
+  
 };
 export default WeeklyCalendar;
