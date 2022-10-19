771915cc072bd144d169dacc8af5470827c20307
sonia <sonialalani7860@gmail.com>
2022-10-19 14:56:00 +0100

    so

--- hackaton/components/buttons/timebuttons.js
+++ hackaton/components/buttons/timebuttons.js
@@ -8,7 +8,9 @@ import styles from "./Buttons.module.css";
 const Buttons = ({ children, ...props }) => {
   return (
     <div>
-      <button {...props} className={styles.button}></button>
+      <button {...props} className={styles.button}>
+        {children}
+      </button>
     </div>
   );
 };
