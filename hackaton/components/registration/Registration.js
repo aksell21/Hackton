2850dce21482ecd09fe6c5ad6308ec6c73918a9a
Compl3x13 <steamds13@gmail.com>
2022-10-19 17:16:02 +0100

    New things

--- hackaton/components/registration/Registration.js
+++ hackaton/components/registration/Registration.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Children } from "react";
 import Buttons from "../buttons/Buttons";
 import styles from "./Registration.module.css"
@@ -23,55 +22,3 @@ export default function Register() {
     </div>
   );
 }
-=======
-import styles from "./Registration.module.css";
-
-export default function Register() {
-  return (
-    <div
-      className="register"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <h1> Registration</h1>
-      <form>
-        <div className="registerName">
-          Username:
-          <br />
-          <input id="username" name="username" placeholder="usrename" />
-          <br />
-          Email:
-          <br />
-          <input id="email" name="email" placeholder="Email" />
-          <br />
-          Password:
-          <br />
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="password"
-          />
-          <br />
-          Confirm Password:
-          <br />
-          <input
-            type="password"
-            id="confirmPassword"
-            name="confirmPassword"
-            placeholder="Confirm Password"
-          />
-          <br />
-          <br />
-          <button type="submit" onClick={() => alert("Account Registered")}>
-            Register
-          </button>
-        </div>
-      </form>
-    </div>
-  );
-}
->>>>>>> 8d476329c02d96025e5471f7eee2f46dcca24480
