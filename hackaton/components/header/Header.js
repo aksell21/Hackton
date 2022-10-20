8beaa5a1661b84608d6d4f0f05ac8b08efd2e16a
Compl3x13 <steamds13@gmail.com>
2022-10-20 02:08:56 +0100

    Correcting the merge in Header and in Users

--- hackaton/components/header/Header.js
+++ hackaton/components/header/Header.js
@@ -8,13 +8,8 @@ export default function Header() {
     return (
         <div>
             <div className={styles.headBar}>
-<<<<<<< HEAD
-                <Link href="/login"><span>Odyssey</span></Link>
-                <Link href="/login"><Buttons>Login</Buttons ></Link>
-=======
                 <Link href="/"><span>Odyssey</span></Link>
                 <Link href="/"><Buttons>Login</Buttons ></Link>
->>>>>>> 31eba1eb4dc8cad8160af710def7cb075aa5b3c0
             </div>
             <div className={styles.headSmallBar}></div>
         </div>
